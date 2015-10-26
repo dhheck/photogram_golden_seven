4b64d68872add525b1497a7a9d9c832a029afb23
dhheck <dheck@chicagobooth.edu>
2015-10-26 16:29:14 -0500

    DHH Delete Complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,28 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+  def destroy
+    p = Photo.find(params[:id])
+    p.destroy
+
+    redirect_to("/photos")
+  end
 
 end
